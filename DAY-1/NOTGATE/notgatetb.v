4e65eabd92b6d37df1b7534dc575e6b038430c1a
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-08 18:27:30 +0530

    Create notgatetb.v

---
+++ DAY-1/NOTGATE/notgatetb.v
@@ -0,0 +1,15 @@
+module notgatetb;
+ reg a;
+ wire y;
+notgate dut(a,y);
+initial begin
+$monitor("a=%b,y=%b",a,y);
+ a=0;#5;
+ a=1;#5;
+ //$display("a=%b,y=%b",a,y);
+ end
+ initial begin
+         $dumpfile("notgate.vcd");
+         $dumpvars;
+ end
+endmodule

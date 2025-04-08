979004026c62c10d2833412d6f339f5309744031
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-08 17:28:54 +0530

    Create and_tb.v

---
+++ DAY-1/AND_GATE/and_tb.v
@@ -0,0 +1,18 @@
+module andtb;
+ reg a,b;
+ wire y;
+ andgate tb(a,b,y);
+ initial begin
+         $monitor("a=%b,b=%b,y=%b",a,b,y);
+
+ a=0; b=0;#5;
+ a=0; b=1;#5;
+ a=1; b=0;#5;
+ a=1; b=1;#5;
+//$display("a=%b,b=%b,y=%b",a,b,y);
+ end
+ initial begin
+         $dumpfile("andgate.vcd");
+         $dumpvars;
+ end
+endmodule

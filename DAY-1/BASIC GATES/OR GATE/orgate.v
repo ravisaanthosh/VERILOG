98f086c8071e4e83c929162db6eb0d8eb2fb036f
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-10 16:49:02 +0530

    Create orgate.v

---
+++ DAY-1/BASIC GATES/OR GATE/orgate.v
@@ -0,0 +1,15 @@
+Data flow modeling  //Data flow modeling 
+module orgate(input a,b, output y);
+ assign y=a|b;
+ endmodule
+
+structural modeling  //structural modeling
+module orgate(input a,b,output y);
+or oragte(y,a,b);
+endmodule
+behavioral modeling  //behavioral modeling 
+module orgate(input a,b,output reg y);
+always @ (a or b) begin;
+y= a|b;
+end
+endmodule

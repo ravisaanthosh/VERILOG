cdeec76ccc85a357710e55b74982c8d75ce1fbfe
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-10 11:04:51 +0530

    Update notgate.v

--- DAY-1/NOTGATE/notgate.v
+++ DAY-1/NOTGATE/notgate.v
@@ -1,3 +1,16 @@
-module notgate(input a, output y);
+
+data flow modeling
+module notgate(input a,output y);
  assign y=~a;
- endmodule
+endmodule
+
+structural modeling 
+module notgate(input a,output y);
+ not n1(y,a);
+endmodule
+
+behavioral modeling
+module notgate(input a,output reg y);
+always @ (a)
+y=~a;
+endmodule

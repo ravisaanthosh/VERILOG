0760c869a976e91b49cfdd7deca533006a847f62
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-10 15:22:58 +0530

    Update andgate.v

--- DAY-1/AND_GATE/andgate.v
+++ DAY-1/AND_GATE/andgate.v
@@ -1,5 +1,15 @@
+dataflow modeling                       //dataflow modeling
+module andgate(input a,b,output y);  
+assign y=a&b;
+endmodule
+
+structural modeling              //structural modeling
 module andgate(input a,b,output y);
+and gate(y,a,b);
+endmodule
 
- assign y=a&b;
-
+behavioral modeling      //behavioral modeling
+module andgate(input a,output reg y);
+always @ (a or b)
+y=a&b;
 endmodule

d0ecd3bc2606072259809287c9d3bfea6a2a206b
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-13 23:12:08 +0530

    Update xorgate.v

--- DAY-1/ARITHMETIC GATES/X-OR GATE/xorgate.v
+++ DAY-1/ARITHMETIC GATES/X-OR GATE/xorgate.v
@@ -2,10 +2,12 @@ Data flow model
 module exor(input a,b,output y);
 assign y=(a^b);
 endmodule
+
 structural model
 module exor(input a,b,output y);
 xor tb(y,a,b);
 endmodule
+
 behavioral model
 module exor(input a,b,output y);
 always @(a or b);

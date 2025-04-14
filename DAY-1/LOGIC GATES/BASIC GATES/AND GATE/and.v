6b51bc9e2b0761f34f4167ef85f6163c336f6c1c
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 08:30:58 +0530

    Update and.v

--- DAY-1/LOGIC GATES/BASIC GATES/AND GATE/and.v
+++ DAY-1/LOGIC GATES/BASIC GATES/AND GATE/and.v
@@ -1,1 +1,15 @@
+Dataflow modeling                       //dataflow modeling
+module andgate(input a,b,output y);  
+assign y=a&b;
+endmodule
 
+structural modeling              //structural modeling
+module andgate(input a,b,output y);
+and gate(y,a,b);
+endmodule
+
+behavioral modeling      //behavioral modeling
+module andgate(input a,b,output reg y);
+always @ (a or b)
+y=a&b;
+endmodule

978ecb06a8e5df09ed0dd5ea84030f2175028fe7
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 08:33:52 +0530

    Update orgate.v

--- DAY-1/LOGIC GATES/BASIC GATES/OR GATE/orgate.v
+++ DAY-1/LOGIC GATES/BASIC GATES/OR GATE/orgate.v
@@ -1,1 +1,16 @@
+Data flow modeling  
+module orgate(input a,b, output y);
+ assign y=a|b;
+ endmodule
 
+structural modeling  
+module orgate(input a,b,output y);
+or oragte(y,a,b);
+endmodule
+
+behavioral modeling  
+module orgate(input a,b,output reg y);
+always @ (a or b) begin;
+y= a|b;
+end
+endmodule

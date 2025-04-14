b4e9aadcb4729e83619bafaed4c0db49af9ac767
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 08:38:41 +0530

    Update xnor.v

--- DAY-1/LOGIC GATES/ARITHMETIC GATES/EX-NOR GATE/xnor.v
+++ DAY-1/LOGIC GATES/ARITHMETIC GATES/EX-NOR GATE/xnor.v
@@ -1,1 +1,15 @@
+Data flow model
+module exnor(input a,b,output y);
+assign y=~(a^b);
+endmodule
 
+structural model
+module exnor(input a,b,output y);
+xnor gate(y,a,b);
+endmodule
+
+behavioral model
+module exnor(input a,b,output y);
+always @(a or b);
+assign y=~(a^b);
+endmodule

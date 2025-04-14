fa99a92b46f63078a07e5a17178c32f926cd5f67
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 08:36:41 +0530

    Update nandtb.v

--- DAY-1/LOGIC GATES/UNIVERSAL GATE/NAND GATE/nandtb.v
+++ DAY-1/LOGIC GATES/UNIVERSAL GATE/NAND GATE/nandtb.v
@@ -1,1 +1,17 @@
-
+module nandtb;
+reg a,b;
+wire y;
+nandgate uut(a,b,y);
+initial begin
+$monitor("a=%b,b=%b,y=%b",a,b,y);
+a=0; b=0;
+#5a=0; b=0;
+#5a=0; b=1;
+#5a=1; b=0;
+#5a=1; b=1;
+end 
+initial begin
+ $dumpfile("nandttb.vcd");
+ $dumpvars;
+end
+endmodule

d5698d48f7625cbdf4c069bc78cd5bfc14c3e23c
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 08:40:24 +0530

    Update xortb.v

--- DAY-1/LOGIC GATES/ARITHMETIC GATES/EX-OR GATE/xortb.v
+++ DAY-1/LOGIC GATES/ARITHMETIC GATES/EX-OR GATE/xortb.v
@@ -1,1 +1,17 @@
+module xortb;
+ reg a,b;
+ wire y;
+ exor tb(a,b,y);
+ initial begin
+         $monitor("a=%b,b=%b,y=%b",a,b,y);
 
+ a=0; b=0;#5;
+ a=0; b=1;#5;
+ a=1; b=0;#5;
+ a=1; b=1;#5;
+ end
+ initial begin
+         $dumpfile("xor.vcd");
+         $dumpvars;
+ end
+endmodule

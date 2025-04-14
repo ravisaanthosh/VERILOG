d13c981ba39575f3a43f3ffc530c31235e42831a
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 08:39:47 +0530

    Update xor.v

--- DAY-1/LOGIC GATES/ARITHMETIC GATES/EX-OR GATE/xor.v
+++ DAY-1/LOGIC GATES/ARITHMETIC GATES/EX-OR GATE/xor.v
@@ -1,1 +1,15 @@
+Data flow model model
+module exor(input a,b,output y);
+assign y=(a^b);
+endmodule
 
+structural model
+module exor(input a,b,output y);
+xor tb(y,a,b);
+endmodule
+
+behavioral model
+module exor(input a,b,output y);
+always @(a or b);
+assign y=(a^b);
+endmodule

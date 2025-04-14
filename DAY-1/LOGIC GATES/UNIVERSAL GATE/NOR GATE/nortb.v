b5f09883a233504e71785f3d8acd75159cb181d2
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 08:37:48 +0530

    Update nortb.v

--- DAY-1/LOGIC GATES/UNIVERSAL GATE/NOR GATE/nortb.v
+++ DAY-1/LOGIC GATES/UNIVERSAL GATE/NOR GATE/nortb.v
@@ -1,1 +1,17 @@
-
+module nortb;
+reg a,b;
+wire y;
+norgate uut(a,b,y);
+initial begin
+$monitor("a=%b,b=%b,y=%b",a,b,y);
+a=0; b=0;
+#5a=0; b=0;
+#5a=0; b=1;
+#5a=1; b=0;
+#5a=1; b=1;
+end 
+initial begin
+$dumpfile("nortb.vcd");
+$dumpvars;
+end
+endmodule

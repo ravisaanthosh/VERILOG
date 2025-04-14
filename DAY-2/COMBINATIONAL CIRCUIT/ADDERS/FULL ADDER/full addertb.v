52ac1a7a7eeff062b9d5968166925dcfba38f2ad
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 20:00:12 +0530

    Update full addertb.v

--- DAY-2/COMBINATIONAL CIRCUIT/ADDERS/FULL ADDER/full addertb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/ADDERS/FULL ADDER/full addertb.v
@@ -1,1 +1,20 @@
+module fatb;
+reg a,b,cin;
+wire sum,carry;
+fa u(.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
+initial begin
+$dumpfile("fa.vcd");
+$dumpvars;
+$monitor($time,"a=%b,b=%b,cin=%b,sum=%b,cout=%b",a,b,sum,cin,cout);
+a=0;b=0;cin=0;
+#5 a=0;b=0;cin=0;
+#5 a=0;b=0;cin=1;
+#5 a=0;b=1;cin=0;
+#5 a=0;b=1;cin=1;
+#5 a=1;b=0;cin=0;
+#5 a=1;b=0;cin=1;
+#5 a=1;b=1;cin=0;
+#5 a=1;b=1;cin=1;
 
+end
+endmodule

8f89f87f3baf74a66c9251caf4da9b255551fdff
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 16:27:58 +0530

    Update half addertb.v

--- DAY-2/COMBINATIONAL CIRCUIT/ADDERS/HALF ADDER/half addertb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/ADDERS/HALF ADDER/half addertb.v
@@ -1,1 +1,15 @@
-
+module hftb;
+reg a,b;
+wire sum,carry;
+hf u(.a(a),.b(b),.sum(sum),.carry(carry));
+initial begin
+$dumpfile("halfadder.vcd");
+$dumpvars;
+$monitor($time,"a=%b,b=%b,sum=%b,carry=%b",a,b,sum,carry);
+a=0;b=0;
+#5a=0;b=0;
+#5a=0;b=1;
+#5a=1;b=0;
+#5a=1;b=1;
+end
+endmodule

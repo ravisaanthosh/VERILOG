60f75fa0ca1fc329d7c9865df7149ca26a960884
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 08:32:08 +0530

    Update not.v

--- DAY-1/LOGIC GATES/BASIC GATES/NOT GATE/not.v
+++ DAY-1/LOGIC GATES/BASIC GATES/NOT GATE/not.v
@@ -1,1 +1,15 @@
+Data flow modeling
+module notgate(input a,output y);
+ assign y=~a;
+endmodule
 
+structural modeling 
+module notgate(input a,output y);
+ not n1(y,a);
+endmodule
+
+behavioral modeling
+module notgate(input a,output reg y);
+always @ (a)
+y=~a;
+endmodule

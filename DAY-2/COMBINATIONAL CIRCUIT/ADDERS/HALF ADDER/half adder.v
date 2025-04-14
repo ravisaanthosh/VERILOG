7b8cce443df8756d3a65e7fb517bbd306db0310c
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-14 16:27:24 +0530

    Update half adder.v

--- DAY-2/COMBINATIONAL CIRCUIT/ADDERS/HALF ADDER/half adder.v
+++ DAY-2/COMBINATIONAL CIRCUIT/ADDERS/HALF ADDER/half adder.v
@@ -1,1 +1,18 @@
+Data flow model
+module hf(input a,b,output sum,carry);
+assign sum =a^b;
+assign carry =a&b;
+endmodule
 
+structural model
+module hf(input a,b,output sum,carry);
+xor u(sum,a,b);
+and uu(carry,a,b);
+endmodule
+
+behavioral model
+module hf(input a,b,output sum,carry);
+always @(a or b);
+assign sum=(a^b);
+assign carry=(a&b);
+endmodule

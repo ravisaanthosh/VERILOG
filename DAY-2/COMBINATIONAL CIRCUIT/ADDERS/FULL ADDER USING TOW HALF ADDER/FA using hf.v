a2a8bf1d57e737d07a70a3c5808e94c3a54da0f0
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-15 11:56:33 +0530

    Update FA using hf.v

--- DAY-2/COMBINATIONAL CIRCUIT/ADDERS/FULL ADDER USING TOW HALF ADDER/FA using hf.v
+++ DAY-2/COMBINATIONAL CIRCUIT/ADDERS/FULL ADDER USING TOW HALF ADDER/FA using hf.v
@@ -1,1 +1,11 @@
+module halfadder(input a,b,output sum,carry);
+assign sum=a^b;
+assign carry=a&b;
+endmodule
 
+module fulladder(input a,b,cin,output sum,cout);
+wire x,y,z;
+assign cout=y|z;
+halfadder uut1(.a(a),.b(b),.sum(x),.carry(y));
+halfadder uut2(.a(x),.b (cin),.sum(sum),.carry(z));
+endmodule

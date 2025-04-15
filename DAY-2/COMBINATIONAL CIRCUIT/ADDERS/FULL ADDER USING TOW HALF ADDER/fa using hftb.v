2dd7b7b54a4d06805bd8666e1f278d37a68b060a
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-15 11:57:13 +0530

    Update fa using hftb.v

--- DAY-2/COMBINATIONAL CIRCUIT/ADDERS/FULL ADDER USING TOW HALF ADDER/fa using hftb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/ADDERS/FULL ADDER USING TOW HALF ADDER/fa using hftb.v
@@ -1,1 +1,20 @@
-
+module fulladder_half_tb;
+reg a,b,cin;
+wire sum,cout;
+fulladder uut(.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
+initial
+begin
+$dumpfile("hauf.vcd");
+$dumpvars;
+$monitor($time,"a=%b,b=%b,cin=%b,sum=%b,cout=%b",a,b,cin,sum,cout);
+a=0; b=0;cin=0;
+#5 a=0;b=0;cin=0;
+#5 a=0;b=0;cin=1;
+#5 a=0;b=1;cin=0;
+#5 a=0;b=1;cin=1;
+#5 a=1;b=0;cin=0;
+#5 a=1;b=0;cin=1;
+#5 a=1;b=1;cin=0;
+#5 a=1;b=1;cin=1;
+end
+endmodule

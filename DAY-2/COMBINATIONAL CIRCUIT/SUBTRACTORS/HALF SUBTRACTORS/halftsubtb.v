615749580985d6624b7198f95ed212d75bbca63a
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-15 15:26:25 +0530

    Update halftsubtb.v

--- DAY-2/COMBINATIONAL CIRCUIT/SUBTRACTORS/HALF SUBTRACTORS/halftsubtb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/SUBTRACTORS/HALF SUBTRACTORS/halftsubtb.v
@@ -1,1 +1,15 @@
-
+module halfsubtb;
+reg a,b;
+wire df,bw;
+halfsub v(.a(a),.b(b),.df(df),.bw(bw));
+initial begin
+$dumpfile("hatb.vcd");
+$dumpvars;
+$monitor($time,"a=%b,b=%b,df=%b,bw=%b",a,b,df,bw);
+a=0;b=0;
+#5a=0;b=0;
+#5a=0;b=1;
+#5a=1;b=0;
+#5a=1;b=1;
+end
+endmodule

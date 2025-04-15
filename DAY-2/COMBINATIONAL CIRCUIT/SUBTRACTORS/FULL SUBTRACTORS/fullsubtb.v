2644224f0c123248e4a262c1b5a859439c5f5d66
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-15 16:57:56 +0530

    Update fullsubtb.v

--- DAY-2/COMBINATIONAL CIRCUIT/SUBTRACTORS/FULL SUBTRACTORS/fullsubtb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/SUBTRACTORS/FULL SUBTRACTORS/fullsubtb.v
@@ -1,1 +1,19 @@
-
+module fullsubtb;
+reg a,b,cin;
+wire df,bw;
+fullsub u(.a(a),.b(b),.cin(cin),.df(df),.bw(bw));
+initial begin
+$dumpfile("fs.vcd");
+$dumpvars;
+$monitor($time,"a=%b,b=%b,cin=%b,df=%b,bw=%b",a,b,cin,df,bw);
+a=0; b=0; cin=0;
+#5 a=0; b=0; cin=0;
+#5 a=0; b=0; cin=1;
+#5 a=0; b=1; cin=0;
+#5 a=0; b=1; cin=1;
+#5 a=1; b=0; cin=0;
+#5 a=1; b=0; cin=1;
+#5 a=1; b=1; cin=0;
+#5 a=1; b=1; cin=1;
+end
+endmodule

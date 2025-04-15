854d81314dae44eb0a956179f687827057d0b3b8
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-15 16:57:23 +0530

    Update fullsub.v

--- DAY-2/COMBINATIONAL CIRCUIT/SUBTRACTORS/FULL SUBTRACTORS/fullsub.v
+++ DAY-2/COMBINATIONAL CIRCUIT/SUBTRACTORS/FULL SUBTRACTORS/fullsub.v
@@ -1,1 +1,20 @@
+Data flow model
+module fullsub(input a,b,cin,output df,bw);
+assign df=(a^b^cin);
+assign bw=(~a)&cin|(~a)&b|b&cin;
+endmodule
+
+structural model
+module fullsub(input a,b,cin,output df,bw);
+xor g0(df,a,b,cin);
+  and g2(san1,~a,cin,~a,b,b,cin); //output will be mismatch fix later
+assign bw=san1;
+endmodule
+
+behavioral model
+module fullsub(input a,b,cin,output df,bw);
+always @(a or b or cin);
+assign df=(a^b^cin);
+assign bw=(~a)&cin|(~a)&b|b&cin;
+endmodule
 

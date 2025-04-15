d4b90ab2104b2853565f1f905e2782ae59806f3a
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-15 15:25:47 +0530

    Update halfsub.v

--- DAY-2/COMBINATIONAL CIRCUIT/SUBTRACTORS/HALF SUBTRACTORS/halfsub.v
+++ DAY-2/COMBINATIONAL CIRCUIT/SUBTRACTORS/HALF SUBTRACTORS/halfsub.v
@@ -1,1 +1,19 @@
+Data flow model
+module halfsub(input a,b,output df,bw);
+assign df=(a^b);
+assign bw=~(a)&b;
+endmodule
 
+structural model
+module halfsub(input a,b,output df,bw);
+xor g0(df, a, b);
+  and g1(ndot, ~a, b);  //actual formula 'ab so its represent in temp signal like ndot ,~a,b after to assign bw=ndot; note only indertimine mingle gate values only
+assign bw = ndot;
+endmodule
+
+behavioral model
+module halfsub(input a,b,output df,bw);
+always @(a or b);
+assign df=(a^b);
+assign bw=~(a)&b;
+endmodule

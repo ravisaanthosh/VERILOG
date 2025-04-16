f3599396bb1f207704cc146522031606f687d6d8
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-16 10:31:14 +0530

    Update 3 to 8 dc.v

--- DAY-2/COMBINATIONAL CIRCUIT/DECODERS/3 to 8 DECODER/3 to 8 dc.v
+++ DAY-2/COMBINATIONAL CIRCUIT/DECODERS/3 to 8 DECODER/3 to 8 dc.v
@@ -1,1 +1,13 @@
+Data flow model
+module dc3to8(input wire [2:0] in, input wire en,output [7:0] out);
+assign out[0]=en & ~in[2] & ~in[1] & ~in[0];
+assign out[1]=en & ~in[2] & ~in[1] & in[0];
+assign out[2]=en & in[2] & ~in[1] & in[0];
+assign out[3]=en & in[2] & ~in[1] & ~in[0];
+assign out[4]=en & in[2] & ~in[1] & ~in[0];
+assign out[5]=en & in[2] & ~in[1] & in[0];
+assign out[6]=en & in[2] & in[1] & ~in[0];
+assign out[7]=en & in[2] & in[1] & in[0];
+endmodule
 
+//behavioral model sooon

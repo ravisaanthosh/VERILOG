77c42124c226b4c032bdad85980890c296f68dbe
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-16 14:56:06 +0530

    Update entb.v

--- DAY-2/COMBINATIONAL CIRCUIT/ENCODERS/4 TO 2 ENCODER/entb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/ENCODERS/4 TO 2 ENCODER/entb.v
@@ -1,1 +1,14 @@
-
+module entb;
+reg[3:0] in;
+wire[1:0] out;
+en uut(.in(in),.out(out));
+initial begin
+$dumpfile("dc.vcd");
+$dumpvars;
+$monitor($time,"in=%b,out=%b",in,out);
+#5 in = 4'b0001;
+#5 in = 4'b0010;
+#5 in = 4'b0100;
+#5 in = 4'b1000;
+end
+endmodule

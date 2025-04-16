cc5f68357858a66a34496ca98927cbb7210ae287
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-16 21:13:42 +0530

    Update epbtb.v

--- DAY-2/COMBINATIONAL CIRCUIT/EVEN PARITY BIT GENERATOR/epbtb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/EVEN PARITY BIT GENERATOR/epbtb.v
@@ -1,1 +1,26 @@
+op h(.a(a),.b(b),.c(c),.p(p),.e(e));
+initial begin
+$dumpfile("op.vcd");
+$dumpvars;
+$monitor($time,"a=%b,b=%b,c=%b,p=%b,e=%b",a,b,c,p,e);
+a=0; b=0; c=0; p=0; 
+#5 a=0; b=0; c=0; p=0; 
+#5 a=0; b=0; c=0; p=1; 
+#5 a=0; b=0; c=1; p=0; 
+#5 a=0; b=0; c=1; p=1; 
+#5 a=0; b=1; c=0; p=0; 
+#5 a=0; b=1; c=0; p=1; 
+#5 a=0; b=1; c=1; p=0; 
+#5 a=0; b=1; c=1; p=1; 
+#5 a=1; b=0; c=0; p=0; 
+#5 a=1; b=0; c=0; p=1; 
+#5 a=1; b=0; c=1; p=0; 
+#5 a=1; b=0; c=1; p=1; 
+#5 a=1; b=1; c=0; p=0; 
+#5 a=1; b=1; c=0; p=1; 
+#5 a=1; b=1; c=1; p=0; 
+#5 a=1; b=1; c=1; p=1; 
 
+
+end
+endmodule

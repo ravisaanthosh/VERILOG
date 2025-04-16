fbd3f927a131da9264a0c4e656fbbfe64df24906
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-16 09:43:37 +0530

    Update 2to4dc.v

--- DAY-2/COMBINATIONAL CIRCUIT/DECODERS/2 to 4 DECODER/2to4dc.v
+++ DAY-2/COMBINATIONAL CIRCUIT/DECODERS/2 to 4 DECODER/2to4dc.v
@@ -1,1 +1,6 @@
-
+module dc2to4(input wire [1:0] in, input wire en,output [3:0] out);
+assign out[0]= en & ~in[1] & ~in[0];
+assign out[1]= en & in[1] & ~in[0];
+assign out[2]= en & ~in[1] & in[0];
+assign out[3]= en & in[1] & in[0];
+endmodule

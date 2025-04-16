04be3c21dd567b66745c8daf078f98f77370f9eb
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-16 10:32:05 +0530

    Update 3 to 8dctb.v

--- DAY-2/COMBINATIONAL CIRCUIT/DECODERS/3 to 8 DECODER/3 to 8dctb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/DECODERS/3 to 8 DECODER/3 to 8dctb.v
@@ -1,1 +1,20 @@
-
+module dc3to8tb;
+reg [2:0] in;
+reg en;
+wire [7:0] out;
+dc3to8 h(.in(in),.en(en),.out(out));
+initial begin
+$dumpfile("dc.vcd");
+$dumpvars;
+$monitor($time,"in=%b,en=%b,out=%b",in,en,out);
+ en=0; in=3'b000;
+#5 en=0; in=3'b000;
+#5 en=1; in=3'b001;
+#5 en=1; in=3'b010;
+#5 en=1; in=3'b011;
+#5 en=1; in=3'b100;
+#5 en=1; in=3'b101;
+#5 en=1; in=3'b110;
+#5 en=1; in=3'b111;
+end
+endmodule

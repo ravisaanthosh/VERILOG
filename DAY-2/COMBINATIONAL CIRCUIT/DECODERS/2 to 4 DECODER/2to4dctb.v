98a6541d158f3f282cd66b4a10ce5108e72ce94a
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-16 09:44:11 +0530

    Update 2to4dctb.v

--- DAY-2/COMBINATIONAL CIRCUIT/DECODERS/2 to 4 DECODER/2to4dctb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/DECODERS/2 to 4 DECODER/2to4dctb.v
@@ -1,1 +1,16 @@
-
+module dc2to4tb;
+reg [1:0] in;
+reg en;
+wire [3:0] out;
+dc2to4 h(.in(in),.en(en),.out(out));
+initial begin
+$dumpfile("dc.vcd");
+$dumpvars;
+$monitor($time,"in=%b,en=%b,out=%b",in,en,out);
+#5 en=0; in=2'b00;
+#5 en=1; in=2'b00;
+#5 en=1; in=2'b01;
+#5 en=1; in=2'b10;
+#5 en=1; in=2'b11;
+end
+endmodule

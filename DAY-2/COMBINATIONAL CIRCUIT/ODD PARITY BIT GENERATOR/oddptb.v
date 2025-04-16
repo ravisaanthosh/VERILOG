6c4fc43325324376e55ad908432acee6df360ccf
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-16 21:00:35 +0530

    Update oddptb.v

--- DAY-2/COMBINATIONAL CIRCUIT/ODD PARITY BIT GENERATOR/oddptb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/ODD PARITY BIT GENERATOR/oddptb.v
@@ -1,1 +1,19 @@
-
+module optb;
+reg a,b,c;
+wire o;
+op h(.a(a),.b(b),.c(c),.o(o));
+initial begin
+$dumpfile("op.vcd");
+$dumpvars;
+$monitor($time,"a=%b,b=%b,c=%b,o=%b",a,b,c,o);
+a=0; b=0; c=0;
+#5a=0; b=0; c=0;
+#5a=0; b=0; c=1;
+#5a=0; b=1; c=0;
+#5a=0; b=1; c=1;
+#5a=1; b=0; c=0;
+#5a=1; b=0; c=1;
+#5a=1; b=1; c=0;
+#5a=1; b=1; c=1;
+end
+endmodule

24d1a422f90f3b5a2bcc242027e6109e2db138c0
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-16 15:34:29 +0530

    Update entb.v

--- DAY-2/COMBINATIONAL CIRCUIT/ENCODERS/8 TO 3 ENCODER/entb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/ENCODERS/8 TO 3 ENCODER/entb.v
@@ -1,1 +1,18 @@
-
+module entb;
+reg[7:0] in;
+wire[2:0] out;
+en uut(.in(in),.out(out));
+initial begin
+$dumpfile("dc.vcd");
+$dumpvars;
+$monitor($time,"in=%b,out=%b",in,out);
+#5 in = 8'b000001;
+#5 in = 8'b000010;
+#5 in = 8'b000100;
+#5 in = 8'b101000;
+#5 in = 8'b100000;
+#5 in = 8'b100000;
+#5 in = 8'b100000;
+#5 in = 8'b100000;
+end
+endmodule

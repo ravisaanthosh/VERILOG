c370a77751897da832938cdb9ca470865c61f8b2
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-17 13:41:17 +0530

    Update muxtb.v

--- DAY-3/MUX/2:1mux/muxtb.v
+++ DAY-3/MUX/2:1mux/muxtb.v
@@ -1,1 +1,16 @@
+module muxtb;
+reg i0,i1,sel;
+wire y;
+mux h(.i0(i0),.i1(i1),.sel(sel),.y(y));
+initial begin
+$dumpfile("mux.vcd");
+$dumpvars;
+$monitor($time,"i0=%b,i1=%b,sel=%b,y=%b",i0,i1,sel,y);
+i0=0; i1=0; sel=0;
+#5i0=1; sel=0;
+#5i1=1; sel=1;
+#5i0=0; sel=0;
+#5i1=0; sel=1;
+end
+endmodule
 

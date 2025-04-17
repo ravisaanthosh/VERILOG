9eac9cb8d038b6b0f62d37681a9dde4256885ad8
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-17 20:43:42 +0530

    Update mtb.v

--- DAY-3/MUX/4:1 MUX/mtb.v
+++ DAY-3/MUX/4:1 MUX/mtb.v
@@ -1,1 +1,12 @@
-
+module muxtb;
+reg s0,s1,a0,a1,a2,a3;
+wire y;
+mux h(.s0(s0),.s1(s1),.a0(a0),.a1(a1),.a2(a2),.a3(a3),.y(y));
+initial begin
+$dumpfile("mux.vcd");
+$dumpvars;
+$monitor($time,"s0=%b,s1=%b,a0=%b,a1=%b,a2=%b,a3=%b,y=%b",s0,s1,a0,a1,a2,a3,y);
+s1=1;s0=0;a0=0;a1=0;a2=1;a3=0;
+#5s1=1;s0=0;a0=0;a1=0;a2=1;a3=1;
+end
+endmodule

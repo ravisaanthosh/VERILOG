cd8dc7754a067624d220ac9353cd39cdce39ca00
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-21 16:53:23 +0530

    Update bgtb.v

--- DAY-3/CODE CONVERTERS/BINARY TO GRAY/bgtb.v
+++ DAY-3/CODE CONVERTERS/BINARY TO GRAY/bgtb.v
@@ -1,1 +1,26 @@
-
+module bgtb;
+reg [3:0] b;
+wire [3:0] g;
+bg uut(.b(b),.g(g));
+initial begin
+$dumpfile("bg.vcd");
+$dumpvars;
+$monitor($time,"b=%b,g=%b",b,g);
+b=4'b0000;
+#5 b=4'b0001;
+#5 b=4'b0010;
+#5 b=4'b0011;
+#5 b=4'b0100;
+#5 b=4'b0101;
+#5 b=4'b0110;
+#5 b=4'b0111;
+#5 b=4'b1000;
+#5 b=4'b1001;
+#5 b=4'b1010;
+#5 b=4'b1011;
+#5 b=4'b1100;
+#5 b=4'b1101;
+#5 b=4'b1110;
+#5 b=4'b1111;
+end
+endmodule

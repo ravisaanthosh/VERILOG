3d7ba2d0a3cbc1b5a0f6c84e54d028a3c9d1c05d
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-21 09:29:35 +0530

    Update dm.v

--- DAY-3/DE-MUX/1:2DEMUX/dm.v
+++ DAY-3/DE-MUX/1:2DEMUX/dm.v
@@ -2,3 +2,8 @@ module d(input s,i,output y1,y0);
 assign y1=(s&i);
 assign y0=(~s&i);
 endmodule
+
+concatenation method
+module d(input s,i,output y1,y0);
+assign {y1,y0}=s?{1'b0,i}:{i,1'b0};
+endmodule

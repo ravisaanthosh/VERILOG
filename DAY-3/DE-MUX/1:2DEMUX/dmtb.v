c5fd6acf89ec7ad506a4a279c2442e5cbefd536f
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-21 09:05:17 +0530

    Update dmtb.v

--- DAY-3/DE-MUX/1:2DEMUX/dmtb.v
+++ DAY-3/DE-MUX/1:2DEMUX/dmtb.v
@@ -1,1 +1,12 @@
-
+module dtb;
+reg s,i;
+wire y1,y0;
+d uut(.s(s),.i(i),.y1(y1),.y0(y0));
+initial begin
+$dumpfile("d.vcd");
+$dumpvars;
+$monitor ($time,"s=%b,i=%b,y1=%b,y0=%b",s,i,y1,y0);
+s=0;i=1;
+#5 s=1;i=0;
+end
+endmodule

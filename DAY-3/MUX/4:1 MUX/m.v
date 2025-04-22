431c566bdb4023a9f836bdbbecfde2ecfb70be21
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-22 10:03:09 +0530

    Update m.v

--- DAY-3/MUX/4:1 MUX/m.v
+++ DAY-3/MUX/4:1 MUX/m.v
@@ -2,6 +2,8 @@ ternary operator
 module mux(input s0,s1,a0,a1,a2,a3,output y);
 assign y= s0?(s1? a3:a1):(s1? a2:a0);
 endmodule
+
+
 behavioral model
 module mux(input s0,s1,a0,a1,a2,a3,output reg y);
 always begin @(*)

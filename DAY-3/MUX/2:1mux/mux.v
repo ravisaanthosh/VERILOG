64c54f6a3392d1c90938555a88ced4fd6e6c031b
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-22 09:46:28 +0530

    Update mux.v

--- DAY-3/MUX/2:1mux/mux.v
+++ DAY-3/MUX/2:1mux/mux.v
@@ -6,3 +6,13 @@ using ternary operator
 module mux(input i0,i1,sel,output y);
 assign y= sel ? i0:i1;
 endmodule
+
+behavioral model
+module mux(input i0,i1,sel,output reg y);
+always begin @ (sel or i0 or i1);
+	if (sel==1)
+		y=i1;
+	else
+		y=i0;
+end
+endmodule

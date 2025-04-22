915c9adf2ae7dbc83f380a7d1c7ddcf370fd1ca6
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-22 10:02:24 +0530

    Update m.v

--- DAY-3/MUX/4:1 MUX/m.v
+++ DAY-3/MUX/4:1 MUX/m.v
@@ -1,3 +1,16 @@
 module mux(input s0,s1,a0,a1,a2,a3,output y);
 assign y= s0?(s1? a3:a1):(s1? a2:a0);
 endmodule
+behavioral model
+module mux(input s0,s1,a0,a1,a2,a3,output reg y);
+always begin @(*)
+	if (s0==0 & s1==0)
+		y=a0;
+	else if (s0==0 & s1==1)
+		y=a1;
+	else if (s0==1 & s1==0)
+		y=a2;
+	else
+		y=a3;
+end
+endmodule

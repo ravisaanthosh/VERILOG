c75eec5aee8873ee2b1656a6f7c4185875f5da20
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-23 13:22:37 +0530

    Update xsb.v

--- DAY-3/CODE CONVERTERS/XS-3 CODE/XS-3 TO BINARY/xsb.v
+++ DAY-3/CODE CONVERTERS/XS-3 CODE/XS-3 TO BINARY/xsb.v
@@ -1,18 +1,18 @@
-//BCD to XS-3 code
-module xs(input [3:0] bcd,output reg[3:0] xs);
+//XS-3 TO BCD CONVERTER
+module xs(input [3:0] XS,output reg[3:0] BCD);
 always @ (*) begin
-case (xs)
-4'b0011: xs = 4'b0000;
-            4'b0100: xs = 4'b0001;
-            4'b0101: xs = 4'b0010;
-            4'b0110: xs = 4'b0011;
-            4'b0111: xs = 4'b0100;
-            4'b1000: xs = 4'b0101;
-            4'b1001: xs = 4'b0110;
-            4'b1010: xs = 4'b0111;
-            4'b1011: xs = 4'b1000;
-            4'b1100: xs = 4'b1001;
-            default: xs = 4'bxxxx;
+            case (BCD)
+4'b0011: BCD = 4'b0000;
+            4'b0100: BCD = 4'b0001;
+            4'b0101: BCD = 4'b0010;
+            4'b0110: BCD = 4'b0011;
+            4'b0111: BCD = 4'b0100;
+            4'b1000: BCD = 4'b0101;
+            4'b1001: BCD = 4'b0110;
+            4'b1010: BCD = 4'b0111;
+            4'b1011: BCD = 4'b1000;
+            4'b1100: BCD = 4'b1001;
+            default: BCD = 4'bxxxx;
 endcase
 end
 endmodule

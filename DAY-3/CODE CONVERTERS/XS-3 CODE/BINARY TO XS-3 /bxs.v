aa29fffe643d06527bcd03caddd7258874255539
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-23 14:31:32 +0530

    Update bxs.v

--- DAY-3/CODE CONVERTERS/XS-3 CODE/BINARY TO XS-3 /bxs.v
+++ DAY-3/CODE CONVERTERS/XS-3 CODE/BINARY TO XS-3 /bxs.v
@@ -1,1 +1,17 @@
+module sx(input [3:0] bcd,output reg[3:0] xs);
+always @ (*) begin
+case (bcd)
+4'b0000:     xs=4'b0011;
+4'b0001:     xs=4'b0100;
+4'b0010:     xs=4'b0101;
+4'b0011:     xs=4'b0110;
+4'b0101:     xs=4'b0111;
+4'b0110:     xs=4'b1000;
+4'b0111:     xs=4'b1001;
+4'b1000:     xs=4'b1010;
+4'b1001:     xs=4'b1011;
+default:     xs=4'b0000;
+endcase
+end
+endmodule
 

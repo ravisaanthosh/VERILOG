324b335e98273a7dfed9e4d0eede56c28cbb10ee
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-23 13:24:55 +0530

    Update xstb.v

--- DAY-3/CODE CONVERTERS/XS-3 CODE/XS-3 TO BINARY/xstb.v
+++ DAY-3/CODE CONVERTERS/XS-3 CODE/XS-3 TO BINARY/xstb.v
@@ -1,28 +1,28 @@
 module xstb;
-reg [3:0] bcd;
-wire [3:0] xs;
+ reg [3:0] XS;
+ wire [3:0] BCD;
 xs uut(.bcd(bcd),.xs(xs));
 initial begin
 $dumpfile ("xs.vcd");
 $dumpvars;
-$monitor ($time,"bcd=%b,xs=%b",bcd,xs);
- bcd = 4'b0000;
+ $monitor ($time,"XS=%b,BCD=%b",XS,BCD);
+ XS = 4'b0000;
 
-          #2 bcd = 4'b0011;
-          #2 bcd = 4'b0100;
-          #2 bcd = 4'b0101;
-          #2 bcd = 4'b0110;
-          #2 bcd = 4'b0111;
-          #2 bcd = 4'b1000;
-          #2 bcd = 4'b1001;
-          #2 bcd = 4'b1010;
-          #2 bcd = 4'b1011;
-          #2 bcd = 4'b1100;
-          #2 bcd = 4'b1101;
-          #2 bcd = 4'b1110;
-          #2 bcd = 4'b1111;
-          #2 bcd = 4'b0000;
-          #2 bcd = 4'b0001;
-          #2 bcd = 4'b0010;
+          #2 XS = 4'b0011;
+          #2 XS = 4'b0100;
+          #2 XS = 4'b0101;
+          #2 XS = 4'b0110;
+          #2 XS = 4'b0111;
+          #2 XS = 4'b1000;
+          #2 XS = 4'b1001;
+          #2 XS = 4'b1010;
+          #2 XS = 4'b1011;
+          #2 XS = 4'b1100;
+          #2 XS = 4'b1101;
+          #2 XS = 4'b1110;
+          #2 XS = 4'b1111;
+          #2 XS = 4'b0000;
+          #2 XS = 4'b0001;
+          #2 XS = 4'b0010;
 end
 endmodule

9cab78792a3a7e0523c60191523d7ad2a911a698
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-24 16:55:02 +0530

    Create 8'bc.v

---
+++ DAY-3/COMPARATOR/8'BIT COMPARATOR USING THREE VARIABLE /8'bc.v
@@ -0,0 +1,13 @@
+Tow variable
+module comp(input [7:0] a,b,output aeb,agb,alb);
+assign aeb=(a==b);
+assign agb=(a>b);
+assign alb=(a<b);
+endmodule
+
+Three variable
+module comp(input [7:0] a,b,c,output aeb,agb,alb);
+assign aeb= ((a==b) && (a ==c ) && (b == c));
+assign agb=(a>b>c);
+assign alb=(a<b<c);
+endmodule

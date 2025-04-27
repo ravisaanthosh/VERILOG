43b47ff507c08d18b2f8123032b932fda49d6421
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-27 16:19:43 +0530

    Update Sr.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/SR LATCH/Sr.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/SR LATCH/Sr.v
@@ -1,1 +1,24 @@
+Data flow model
+module srl(input s,r,output q,qb);
+assign q = ~(r | qb);
+assign qb = ~( s | q);
+endmodule
 
+structural model
+module srl(input s,r,output q,qb);
+nor g1(q,r,qb);
+nor g2(qb,s,q);
+endmodule
+
+behavioral model
+module srl(input s,r,output reg q,output qb);
+assign qb=(~q);
+always @ (*) begin
+	case ({s,r})
+		2'b00:q=q; 		//nochange
+		2'b01:q=1'b0;		//reset
+		2'b10:q=1'b1;		//set
+		2'b11:q=1'bx;		//un determine
+	endcase
+end
+endmodule

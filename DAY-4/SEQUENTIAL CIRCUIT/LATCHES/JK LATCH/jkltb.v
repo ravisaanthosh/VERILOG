c73e049d9b332bbc183a778f01856c320b3647bb
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-27 17:44:16 +0530

    Update jkltb.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/JK LATCH/jkltb.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/JK LATCH/jkltb.v
@@ -1,1 +1,15 @@
-
+module jktb;
+reg j,k;
+wire q,qb;
+jk uut (.j(j),.k(k),.q(q),.qb(qb));
+initial begin
+$dumpfile("jk.vcd");
+$dumpvars;
+$monitor ($time,"j=%b,k=%b,q=%b,qb=%b",j,k,q,qb);
+j=0; k=0;
+#5 j=0; k=0;
+#5 j=1; k=0;
+#5 j=0; k=1;
+#5 j=1; k=1;
+end
+endmodule

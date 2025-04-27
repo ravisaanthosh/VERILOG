3b8816095e81c2a3a20a2fd246f82e3159d35528
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-27 17:02:07 +0530

    Update dtb.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/D LATCH/dtb.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/D LATCH/dtb.v
@@ -1,1 +1,13 @@
-
+module dtb;
+reg d;
+wire q,qb;
+d uut(.d(d),.q(q),.qb(qb));
+initial begin
+$dumpfile("d.vcd");
+$dumpvars;
+$monitor($time,"d=%b,q=%b,qb=%b",d,q,qb);
+d=0;
+#5 d=0;
+#5 d=1;
+end
+endmodule

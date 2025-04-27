d87f772fd59aa9c89cc3c2bc377694f5a96fb8dc
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-27 16:20:12 +0530

    Update srtb.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/SR LATCH/srtb.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/SR LATCH/srtb.v
@@ -1,1 +1,15 @@
-
+module srltb;
+reg s,r;
+wire q,qb;
+srl uut(.s(s),.r(r),.q(q),.qb(qb));
+initial begin
+$dumpfile("sr.vcd");
+$dumpvars;
+$monitor($time,"s=%b,r=%b,q=%b,qb=%b",s,r,q,qb);
+s=0;r=0;
+#5 s=0; r=0;
+#5 s=0; r=1;
+#5 s=1; r=0;
+#5 s=1; r=1;
+end
+endmodule

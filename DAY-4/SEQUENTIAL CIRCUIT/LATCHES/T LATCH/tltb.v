402db5d5f8519f44a47b5375525a65a62a889502
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-27 18:22:13 +0530

    Update tltb.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/T LATCH/tltb.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/T LATCH/tltb.v
@@ -1,1 +1,13 @@
-
+module tb;
+reg t;
+wire q,qb;
+t uut(.t(t),.q(q),.qb(qb));
+initial begin
+$dumpfile("t.vcd");
+$dumpvars;
+$monitor ($time,"t=%b,q=%b,qb=%b",t,q,qb);
+t=0;
+#5 t=0;
+#5 t=1;
+end
+endmodule

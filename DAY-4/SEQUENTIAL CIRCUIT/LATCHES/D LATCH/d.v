957ed383c1dd2f3b5a1b5fee6da6687739b0f190
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-27 17:01:41 +0530

    Update d.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/D LATCH/d.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/D LATCH/d.v
@@ -1,1 +1,23 @@
+Data flow model
+module d (input d,output q,qb);
+assign q= ~(qb | d);
+assign qb= ~(q|(~d));
+endmodule
 
+structural model // expected output will not sastify fix later
+module d (input d,output q,qb);
+not g1(qb,d);
+nor g2(q,qb,d);
+nor g3(qb,q,d);
+endmodule
+
+behavioral model
+module d(input d,output reg q,output qb);
+assign qb=~q;
+always @ (*) begin
+case ({d})
+1'b0:q=1'b0; //reset
+1'b1:q=1'b1; //set
+endcase
+end
+endmodule

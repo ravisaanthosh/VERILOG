15f9f9b4e4e8945fa28cbe4c509bb84133b1ff76
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-29 14:43:24 +0530

    Update d.v

--- DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/D FLIP FLOP/d.v
+++ DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/D FLIP FLOP/d.v
@@ -1,1 +1,13 @@
-
+module dff(input d,clk,rs,output reg q,output qb);
+always @ (posedge clk) begin
+if (rs) begin
+q<=1;
+end
+else
+case ({d})
+1'b0 : q<=1'b0; //reset
+1'b1 : q<=1'b1; //set
+endcase
+end
+assign qb = ~q;
+endmodule

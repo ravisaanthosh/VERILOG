fd5403ab6dd95af692d6ea4ead9914b978f2ee4b
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-29 18:38:46 +0530

    Update t.v

--- DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/T FLIP FLOP/t.v
+++ DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/T FLIP FLOP/t.v
@@ -1,1 +1,22 @@
-
+module tfftb;
+reg t,clk,rs;
+wire q,qb;
+tff uut(.t(t),.clk(clk),.rs(rs),.q(q),.qb(qb));
+initial begin
+$dumpfile("t.vcd");
+$dumpvars;
+end
+initial begin
+clk=1;
+forever #5clk=~clk;
+end
+initial begin
+#10 t=0; rs=1; rs=0; #15;
+$display($time,"t=%b,q=%b,qb=%b",t,q,qb); #18;
+#20 t=1; #16;
+$display($time,"t=%b,q=%b,qb=%b",t,q,qb); #25;
+#45;
+$finish;
+end
+endmodule
+ 

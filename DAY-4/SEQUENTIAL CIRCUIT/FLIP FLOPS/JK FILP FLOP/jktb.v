e6ae2d8fcacd9875650f7ab7b955fa4c4a76b14e
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-29 16:24:28 +0530

    Update jktb.v

--- DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/JK FILP FLOP/jktb.v
+++ DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/JK FILP FLOP/jktb.v
@@ -1,1 +1,15 @@
-
+module jkff(input j,k,clk,rs,output reg q,output qb);
+always @ (posedge clk) begin
+if (rs) begin
+q<=1;
+end
+else
+case ({j,k})
+2'b00 : q<=q; //nochange
+2'b01 : q<=1'b0; // reset
+2'b10 : q<=1'b1; //set
+2'b11 : q<=1'bx; //toggle
+endcase
+end
+assign qb=~q;
+endmodule

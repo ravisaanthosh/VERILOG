3df79ad8be02fbd75327e821b7857bf32b09a3bb
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-29 14:43:52 +0530

    Update dtb.v

--- DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/D FLIP FLOP/dtb.v
+++ DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/D FLIP FLOP/dtb.v
@@ -1,1 +1,21 @@
-
+module dfftb;
+reg d,clk,rs;
+wire q,qb;
+dff uut(d,clk,rs,q,qb);
+initial begin
+$dumpfile ("d.vcd");
+$dumpvars;
+end
+initial begin
+clk=1;
+forever #5 clk=~clk;
+end
+initial begin
+#10 d=0; rs=1; rs=0; #15;
+$display($time,"d=%b,clk=%b,rs=%b,q=%b,qb=%b",d,clk,rs,q,qb); #5
+#15 d=1; #18;
+$display($time,"d=%b,clk=%b,rs=%b,q=%b,qb=%b",d,clk,rs,q,qb); #10
+#80;
+$finish;
+end
+endmodule

f190e048ecfd8d70961e53fd7dcad4fdd57abde7
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-29 18:39:09 +0530

    Update ttb.v

--- DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/T FLIP FLOP/ttb.v
+++ DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/T FLIP FLOP/ttb.v
@@ -1,1 +1,13 @@
-
+module tff(input t,clk,rs,output reg q,output qb);
+always @ (posedge clk) begin
+if (rs) begin
+q<=1;
+end
+else
+case({t})
+1'b0 : q<=1'b0; //set
+1'b1 : q<=1'b1; //toggle
+endcase
+end
+assign qb=~q;
+endmodule

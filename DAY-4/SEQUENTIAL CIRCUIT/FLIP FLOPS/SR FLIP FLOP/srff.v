17f46646f82bad1e37454c557b0e557f252ccb2e
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-29 13:58:04 +0530

    Update srff.v

--- DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/SR FLIP FLOP/srff.v
+++ DAY-4/SEQUENTIAL CIRCUIT/FLIP FLOPS/SR FLIP FLOP/srff.v
@@ -1,1 +1,15 @@
-
+module srff(input s,r,clk,rs,output reg q,output qb);
+always @ (posedge clk) begin
+	if (rs) begin
+q<=1;
+end
+else
+casex ({s,r})
+2'b00 :q<=q; //no change
+2'b01 :q<=1'b0; //reset
+2'b10 :q<=1'b1; //set
+2'b11 :q<=1'bx; //invalid
+endcase
+end
+assign qb=~q;
+endmodule

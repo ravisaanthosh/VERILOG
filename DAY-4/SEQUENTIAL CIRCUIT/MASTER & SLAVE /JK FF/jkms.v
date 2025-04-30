40cc47c3693804221295605b9bdab2e8396f9c97
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-04-30 18:59:45 +0530

    Update jkms.v

--- DAY-4/SEQUENTIAL CIRCUIT/MASTER & SLAVE /JK FF/jkms.v
+++ DAY-4/SEQUENTIAL CIRCUIT/MASTER & SLAVE /JK FF/jkms.v
@@ -1,1 +1,26 @@
-
+module jkms(input j,k,clk,output reg msq,msqb);
+reg sq,sqb;
+always @ (posedge clk) begin
+if (j==0 && k==0) begin
+sq<=1'bx;
+sqb<=1'bx;
+end
+else if(j==0 && k==1) begin
+sq<=1'b0;
+sqb<=1'b1;
+end
+else if(j==1 && k==0) begin
+sq<=1'b1;
+sqb<=1'b0;
+end
+else begin
+sq<=~sq;
+sqb<=~sqb;
+end
+end
+//slave configuration
+always @ (negedge clk) begin
+msq<=sq;
+msqb<=sqb;
+end
+endmodule

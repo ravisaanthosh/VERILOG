bc791a8acb5d25a2cd16491b102eeab18bbd59ec
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-04 19:28:42 +0530

    Update dtb.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/LATCHES WITH ENABLE/D_EN_LATCH/dtb.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/LATCHES WITH ENABLE/D_EN_LATCH/dtb.v
@@ -1,1 +1,15 @@
-
+module dtb;
+reg d,en;
+wire q,qb;
+den uut(.d(d),.en(en),.q(q),.qb(qb));
+initial begin
+$dumpfile("d.vcd");
+$dumpvars;
+$monitor($time,"d=%b,en=%b,q=%b,qb=%b",d,en,q,qb);
+d=0; en=1; 
+#5 d=0;   en=0;
+#15 d=0;  en=0;
+#25 d=1;  en=0;
+#35 d=1;  en=1;
+end
+endmodule

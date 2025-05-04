bf4afa797eb52b0726e32ff0103ff5e4159284c8
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-04 19:28:12 +0530

    Update d.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/LATCHES WITH ENABLE/D_EN_LATCH/d.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/LATCHES WITH ENABLE/D_EN_LATCH/d.v
@@ -1,1 +1,13 @@
-
+module den(input d,en,output reg q,output qb);
+assign qb=~q;
+always @ (*)begin
+if (en) begin
+q<=0;
+end
+else
+case({d})
+1'b0:q<=1'b0;
+1'b1:q<=1'b1;
+endcase
+end
+endmodule

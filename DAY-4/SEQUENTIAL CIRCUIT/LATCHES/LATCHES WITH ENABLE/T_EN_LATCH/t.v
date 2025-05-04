2890b271f649344c434ff6079d6bd36047ea133c
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-04 20:33:02 +0530

    Update t.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/LATCHES WITH ENABLE/T_EN_LATCH/t.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/LATCHES WITH ENABLE/T_EN_LATCH/t.v
@@ -1,1 +1,13 @@
-
+module ten(input t,en,output reg q,output qb);
+assign qb=~q;
+always @ (*)begin
+if (en) begin
+q<=0;
+end
+else
+case({t})
+1'b0:q<=1'b1;
+1'b1:q<=1'b0;
+endcase
+end
+endmodule

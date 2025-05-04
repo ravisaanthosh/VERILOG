8e9b31f31621225155b0fc21f13ff9fbb698f166
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-04 19:48:55 +0530

    Update jk.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/LATCHES WITH ENABLE/JK_EN_LATCH/jk.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/LATCHES WITH ENABLE/JK_EN_LATCH/jk.v
@@ -1,1 +1,15 @@
-
+module jken(input j,k,en,output reg q,output qb);
+assign qb=~q;
+always @ (*)begin
+if (en) begin
+q<=0;
+end
+else
+case({j,k})
+2'b00:q<=q;
+2'b01:q<=1'b0;
+2'b10:q<=1'b1;
+2'b11:q<=~q;
+endcase
+end
+endmodule

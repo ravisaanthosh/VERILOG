d869950a09d376fb0b894f03f702aa3e406bf1cf
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-04 17:41:11 +0530

    Update cla.v

--- DAY-2/COMBINATIONAL CIRCUIT/ADDERS/CARRY LOOK-AHEAD ADDER/cla.v
+++ DAY-2/COMBINATIONAL CIRCUIT/ADDERS/CARRY LOOK-AHEAD ADDER/cla.v
@@ -2,19 +2,19 @@
 module cla(input [3:0] a,b,input cin,output [3:0] sum,output cout);
 wire p0,p1,p2,p3,p4,g0,g1,g2,g3;
 wire c0,c1,c2,c3;
-//propagate formula
+//propagate equation
 assign p0=a[0]^b[0];
 assign p1=a[1]^b[2];
 assign p2=a[2]^b[3];
 assign p3=a[3]^b[3];
 
-//generate formula
+//generate equation
 assign g0=a[0] & b[0];
 assign g1=a[1] & b[1];
 assign g2=a[2] & b[2];
 assign g3=a[3] & b[3];
 
-//carry formula
+//carry equation
 assign c0=g0|p0 & cin;
 assign c1=g1 | p1 & g0 | p1 & p0 & cin;
 assign c2=g2 | p2 & g1 | p2 & p1 & g0 | p2 & p1 & p0 & cin;

996b03e0909b82b5e09151a476b980cf6646630f
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-04 18:48:49 +0530

    Update sr.v

--- DAY-4/SEQUENTIAL CIRCUIT/LATCHES/LATCHES WITH ENABLE/SR_EN_LATCH/sr.v
+++ DAY-4/SEQUENTIAL CIRCUIT/LATCHES/LATCHES WITH ENABLE/SR_EN_LATCH/sr.v
@@ -1,1 +1,15 @@
-
+module sren(input s,r,en,output reg q,output qb);
+assign qb=~q;
+always @ (*)begin
+if (en) begin
+q<=0;
+end
+else
+case({s,r})
+2'b00:q<=q;
+2'b01:q<=1'b0;
+2'b10:q<=1'b1;
+2'b11:q<=1'bx;
+endcase
+end
+endmodul

84fb445679bb507bab2c4352d7d5cfc9b12c37f5
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-04 17:40:08 +0530

    Update clatb.v

--- DAY-2/COMBINATIONAL CIRCUIT/ADDERS/CARRY LOOK-AHEAD ADDER/clatb.v
+++ DAY-2/COMBINATIONAL CIRCUIT/ADDERS/CARRY LOOK-AHEAD ADDER/clatb.v
@@ -1,1 +1,16 @@
-
+module clatb;
+reg [3:0] a,b;
+reg cin;
+wire [3:0] sum;
+wire cout;
+cla uut(.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
+initial begin
+$dumpfile("cla.vcd");
+$dumpvars;
+$monitor($time,"a=%b,b=%b,cin=%b,sum=%b,cout=%b",a,b,cin,sum,cout);
+a=4'b0000; b=4'b0000; cin=0;
+#10 a=4'b1111; b=4'b1111; cin=0;
+#15 a=4'b0001; b=4'b0001; cin=0;
+#50 $finish;
+end
+endmodule

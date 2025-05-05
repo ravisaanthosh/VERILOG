db00d23848647f5cd598331108d96c1d38c6bfa5
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-05 20:42:54 +0530

    Update tfftojkfftb.v

--- DAY-4/SEQUENTIAL CIRCUIT/MASTER & SLAVE /MASTER TFF TO SLAVE JK FF/tfftojkfftb.v
+++ DAY-4/SEQUENTIAL CIRCUIT/MASTER & SLAVE /MASTER TFF TO SLAVE JK FF/tfftojkfftb.v
@@ -1,1 +1,27 @@
+module tfftb;
+reg t,clk;
+wire qtm,qtbm;
+tff master(.t(t),.clk(clk),.qtm(qtm),.qtbm(qtbm));
+ins slave(.t(t),.clk(clk),.qtm(qtm),.qtbm(qtbm));
+initial begin
+$dumpfile("ttojk.vcd");
+$dumpvars;
+clk=1;
+forever #5 clk=~clk;
+end
+initial begin
+#5 t=0; #10;
+$display($time,"t=%b,clk=%b,qtm=%b,qtbm=%b",t,clk,qtm,qtbm);
 
+#15 t=0; #20;
+$display($time,"t=%b,clk=%b,qtm=%b,qtbm=%b",t,clk,qtm,qtbm);
+
+#25 t=1; #30;
+$display($time,"t=%b,clk=%b,qtm=%b,qtbm=%b",t,clk,qtm,qtbm);
+
+#35 t=1; #40;
+$display($time,"t=%b,clk=%b,qtm=%b,qtbmb=%b",t,clk,qtm,qtbm);
+#45;
+$finish;
+end 
+endmodule

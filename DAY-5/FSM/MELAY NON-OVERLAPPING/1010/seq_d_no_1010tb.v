2af1577d6a911221701c82e01a2a6c7175e5d8e0
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-08 15:20:22 +0530

    Update seq_d_no_1010tb.v

--- DAY-5/FSM/MELAY NON-OVERLAPPING/1010/seq_d_no_1010tb.v
+++ DAY-5/FSM/MELAY NON-OVERLAPPING/1010/seq_d_no_1010tb.v
@@ -1,1 +1,36 @@
+module sqtb;
+reg in,clk,rst;
+wire out;
+sq uut(in,clk,rst,out);
+initial clk=0;
+always #2 clk=~clk;
+initial begin
+$dumpfile("1010.vcd");
+$dumpvars;
+$monitor($time,"in=%d,clk=%d,rst=%d,out=%d",in,clk,rst,out);
 
+in=0;
+#10 rst =0; #10 rst=1;
+
+#10 in=1; #10;
+//$display ($time,"in=%d,clk=%d,rst=%b,out=%d",in,clk,rst,out);
+
+#10 in=0; #10;
+//$display ($time,"in=%d,clk=%d,rst=%d,out=%d",in,clk,rst,out);
+
+#10 in=1; #10;
+//$display ($time,"in=%d,clk=%d,rst=%d,out=%d",in,clk,rst,out);
+
+#10 in=0; #10;
+//$display ($time,"in=%d,clk=%d,rst=%d,out=%d",in,clk,rst,out);
+
+ #9 in=1; #10;
+//$display ($time,"in=%d,clk=%d,rst=%d,out=%d",in,clk,rst,out);
+
+#10 in=1; #11;
+//$display ($time,"in=%d,clk=%d,rst=%d,out=%d",in,clk,rst,out); 
+
+#75;
+$finish;
+end
+endmodule

5cc8a4399b6e750e54adf8e09b99f55f93fdf594
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-08 15:19:50 +0530

    Update seq_d_no_1010.v

--- DAY-5/FSM/MELAY NON-OVERLAPPING/1010/seq_d_no_1010.v
+++ DAY-5/FSM/MELAY NON-OVERLAPPING/1010/seq_d_no_1010.v
@@ -1,1 +1,25 @@
-
+//expected output will not be sastifying
+module sq(input in,clk,rst,output out);
+parameter a = 4'd1;
+parameter b = 4'd2;
+parameter c = 4'd3;
+parameter d = 4'd4;
+reg [3:0] st,nx_st;
+always @(posedge clk or negedge rst)begin
+if (!rst)begin
+st<=a;
+end
+else
+st<=nx_st;
+end
+always @ (*)begin
+case (st)
+a : nx_st = in ? b : a;
+b : nx_st = in ? b : c;
+c : nx_st = in ? d : a;
+d : nx_st = in ? b : a;
+default : nx_st = a;
+endcase
+end
+assign out = (st==d) && (in==0);
+endmodule

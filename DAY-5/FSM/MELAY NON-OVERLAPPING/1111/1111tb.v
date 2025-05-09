5b633c7cd983668159a88fae39b9bd686c29401d
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-09 10:15:42 +0530

    Update 1111tb.v

--- DAY-5/FSM/MELAY NON-OVERLAPPING/1111/1111tb.v
+++ DAY-5/FSM/MELAY NON-OVERLAPPING/1111/1111tb.v
@@ -1,1 +1,29 @@
+module seqtb;
+reg x,rst,clk;
+wire z;
+seq uut(x,rst,clk,z);
+initial clk=0;
+always #5 clk=~clk;
 
+initial begin
+$monitor($time,"x=%b,rst=%b,clk=%b,z=%b",x,rst,clk,z);
+
+$dumpfile("1111.vcd");
+$dumpvars;
+
+x=1;
+#1 rst=0;
+#2 rst=1;
+
+#4 x=1;
+
+#4 x=1; 
+
+#4 x=1; 
+
+#4 x=1;  
+
+#50
+$finish;
+end
+end

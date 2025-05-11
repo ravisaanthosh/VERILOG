be501edc4a4d9d0b5823f151948d932f996485ce
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-11 19:02:22 +0530

    Update 1111tb.v

--- DAY-7/MOORE/MOORE-NON-OVERLAPPING/1111/1111tb.v
+++ DAY-7/MOORE/MOORE-NON-OVERLAPPING/1111/1111tb.v
@@ -1,1 +1,43 @@
+module sqtb;
+reg in,clk,rst;
+wire out;
+sq uut(in,clk,rst,out);
+initial clk=0;
+always #2 clk=~clk;
+initial begin
+$dumpfile("1111.vcd");
+$dumpvars;
+$monitor($time,"in=%d,clk=%d,rst=%d,out=%d",in,clk,rst,out);
 
+in=0;
+#5 rst =0; #10 rst=1;
+
+#5 in=1; 
+
+#5 in=1; 
+
+#5 in=1; 
+
+#5 in=1; 
+
+#5 in=1; 
+
+#5 in=1;
+
+#5 in=1;
+
+#5 in=1;
+
+#5 in=1;
+
+#5 in=1;
+
+#5 in=1;
+
+
+ 
+
+#15;
+$finish;
+end
+endmodule

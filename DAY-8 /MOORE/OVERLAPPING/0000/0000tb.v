1c433f2756f96fac5b4141394df1c8c3c7f563b3
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-11 19:35:19 +0530

    Update 0000tb.v

--- DAY-8 /MOORE/OVERLAPPING/0000/0000tb.v
+++ DAY-8 /MOORE/OVERLAPPING/0000/0000tb.v
@@ -1,1 +1,43 @@
+module sqtb;
+reg in,clk,rst;
+wire out;
+sq uut(in,clk,rst,out);
+initial clk=0;
+always #2 clk=~clk;
+initial begin
+$dumpfile("0000.vcd");
+$dumpvars;
+$monitor($time,"in=%d,clk=%d,rst=%d,out=%d",in,clk,rst,out);
 
+in=0;
+#1 rst =0; #2 rst=1;
+
+#5 in=0; 
+
+#5 in=1; 
+
+#5 in=0; 
+
+#5 in=1; 
+
+#5 in=0; 
+
+#5 in=0;
+
+#5 in=0;
+
+#5 in=0;
+
+#5 in=0;
+
+#5 in=1;
+
+#5 in=1;
+
+
+ 
+
+#15;
+$finish;
+end
+endmodule

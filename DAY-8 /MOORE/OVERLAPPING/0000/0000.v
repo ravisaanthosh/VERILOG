a96e84b8345e471e57db5ee6129a786a1b21a58a
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-11 19:34:49 +0530

    Update 0000.v

--- DAY-8 /MOORE/OVERLAPPING/0000/0000.v
+++ DAY-8 /MOORE/OVERLAPPING/0000/0000.v
@@ -1,1 +1,26 @@
-
+module sq(input in,clk,rst,output out);
+parameter a = 4'd1;
+parameter b = 4'd2;
+parameter c = 4'd3;
+parameter d = 4'd4;
+parameter e = 4'd5;
+reg [4:0] st,nx_st;
+always @(posedge clk or negedge rst)begin
+if (!rst)begin
+st<=a;
+end
+else
+st<=nx_st;
+end
+always @ (*)begin
+case (st)
+a : nx_st = in ? a : b;
+b : nx_st = in ? a : c;
+c : nx_st = in ? a : d;
+d : nx_st = in ? a : e;
+e : nx_st = in ? a : e;
+default : nx_st = a;
+endcase
+end
+assign out = (st==e) && (in==0);
+endmodule

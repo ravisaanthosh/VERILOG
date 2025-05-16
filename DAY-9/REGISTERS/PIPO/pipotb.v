9cd03c7ca01622c13187f12f42d5fa3fce854c58
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-16 22:58:06 +0530

    Update pipotb.v

--- DAY-9/REGISTERS/PIPO/pipotb.v
+++ DAY-9/REGISTERS/PIPO/pipotb.v
@@ -1,1 +1,21 @@
+module pipotb;
+reg [3:0]pi;
+reg clk,rs;
+wire [3:0]po;
+pipo uut(pi,clk,rs,po);
+initial begin
+clk=1;
+$dumpfile("pipo.vcd");
+$dumpvars;
+$monitor($time,"pi=%b,clk=%b,rs=%b,po=%b",pi,clk,rs,po);
+forever #5 clk=~clk;
+end
 
+initial begin
+rs=1;
+pi=4'b1110; #10
+rs=0;
+pi=4'b1110; #10
+$finish;
+end
+endmodule

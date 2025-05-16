81e68068997ba5cc1a8cf9276f350cda6caba6e2
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-16 18:58:07 +0530

    Update sisotb.v

--- DAY-9/REGISTERS/SISO/sisotb.v
+++ DAY-9/REGISTERS/SISO/sisotb.v
@@ -1,1 +1,35 @@
+module sisotb;
+reg si,clk,rs;
+wire so;
+siso uut(si,clk,rs,so);
+
+initial begin
+clk=1;
+$dumpfile("siso.vcd");
+$dumpvars;
+$monitor($time,"si=%b,clk=%b,rs=%b,so=%b",si,clk,rs,so);
+forever #5 clk=~clk;
+end
+
+initial begin
+rs=1;
+#10 si=0;
+
+rs=0;
+si=1;
+
+//assign test (1110) cases use formula (2n-1) for siso which is need 7 clk pluse
+
+ si=1; #10
+ si=1; #10
+ si=1; #10
+ si=0; #10
+ si=0; #10
+ si=0; #10
+ si=0; #10
+
+
+$finish;
+end
+endmodule
 

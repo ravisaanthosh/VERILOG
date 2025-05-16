5a762ef8ba5cc5f2ac150900856ca77766f0a3a0
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-16 21:24:26 +0530

    Update piso.v

--- DAY-9/REGISTERS/PISO/piso.v
+++ DAY-9/REGISTERS/PISO/piso.v
@@ -1,1 +1,16 @@
+module piso(input [3:0] pi,input clk,rs,load,output reg so);
+reg [3:0] store;
+always @ (posedge clk)begin
+if(rs==1)begin
+store <= 4'b0000;
+end
 
+else if (load!=1)begin
+store <= pi;
+end
+else begin
+so<= store[0];
+store = store >>1;
+end
+end
+endmodule

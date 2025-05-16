aa26da128ff485afb4ac97824e1da998f6a66c90
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-16 21:24:55 +0530

    Update pisotb.v

--- DAY-9/REGISTERS/PISO/pisotb.v
+++ DAY-9/REGISTERS/PISO/pisotb.v
@@ -1,1 +1,25 @@
+module sipotb;
+reg[3:0] pi;
+reg clk,rs,load;
+wire so;
+piso uut(pi,clk,rs,load,so);
+initial begin
+clk=1;
+$dumpfile("piso.vcd");
+$dumpvars;
+$monitor($time,"pi=%b,clk=%b,rs=%b,load=%b,so=%b",pi,clk,rs,load,so);
+forever #5 clk=~clk;
+end
+initial begin
+rs=1; rs=0;
+//test verification  1110
+load = 0;
+pi = 4'b0111; #10
+load = 1 ; #10
+load = 1 ; #10
+load = 1 ; #10
+load = 1 ; #10
+$finish;
+end
+endmodule
 

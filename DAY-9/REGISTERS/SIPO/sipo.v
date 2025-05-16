7f30be8c74176ea075161d16c5933bea9bc49ca3
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-16 19:40:32 +0530

    Update sipo.v

--- DAY-9/REGISTERS/SIPO/sipo.v
+++ DAY-9/REGISTERS/SIPO/sipo.v
@@ -1,1 +1,13 @@
-
+module sipo(input si,clk,rs,output reg[3:0] po);
+reg [3:0] store;
+always @ (posedge clk) begin
+if(rs==1)begin
+store <= 4'd0;
+end
+else begin
+store <= store >>1;
+store [3] <= si;
+end
+po <= store;
+end
+endmodule

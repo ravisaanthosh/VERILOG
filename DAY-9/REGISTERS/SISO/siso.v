42237f766c4f652461d16685e6c47917ce859b80
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-16 18:57:25 +0530

    Update siso.v

--- DAY-9/REGISTERS/SISO/siso.v
+++ DAY-9/REGISTERS/SISO/siso.v
@@ -1,1 +1,13 @@
-
+module siso(input si,clk,rs,output reg so);
+reg [3:0] store;
+always @ (posedge clk) begin
+if (rs==1) begin
+store = 4'd0;
+end
+else begin
+store = store >> 1;
+store [3] <= si;
+so <= store [0];
+end
+end
+endmodule

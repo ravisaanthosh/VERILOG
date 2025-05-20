3cefa3f96b00e2da15103f1b771feac8a297eb52
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-20 19:04:33 +0530

    Update downcountertb.v

--- DAY-11/COUNTERS/DOWN-COUNTER/downcountertb.v
+++ DAY-11/COUNTERS/DOWN-COUNTER/downcountertb.v
@@ -1,1 +1,18 @@
+module dwcountb;
+reg clk,rs;
+wire [2:0] count;
+dwcou uut(clk,rs,count);
+initial begin
+$dumpfile("downcounter.vcd");
+$dumpvars;
+$monitor($time,"clk=%b,rs=%b,count=%b",clk,rs,count);
+clk=0;
+#5 rs=0;
 
+#5 rs=1;
+
+ #75 $finish;
+end
+always #5 clk=~clk;
+
+endmodule

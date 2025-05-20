a2165ff86bd74273397c0f285516f207738cf17d
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-20 18:36:13 +0530

    Update upcountertb.v

--- DAY-11/COUNTERS/UP-COUNTER/upcountertb.v
+++ DAY-11/COUNTERS/UP-COUNTER/upcountertb.v
@@ -1,1 +1,18 @@
+module upcountb;
+reg clk,rs;
+wire [2:0] count;
+upcou uut(clk,rs,count);
+initial begin
+$dumpfile("upcounter.vcd");
+$dumpvars;
+$monitor($time,"clk=%b,rs=%b,count=%b",clk,rs,count);
+clk=0;
+#5 rs=0;
 
+#5 rs=1;
+
+ #80 $finish;
+end
+always #5 clk=~clk;
+
+endmodule

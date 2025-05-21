0293b8cd80e9376502673478ee011c60ee32833e
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-21 18:18:20 +0530

    Update rctb.v

--- DAY-12/COUNTERS/RING COUNTER/rctb.v
+++ DAY-12/COUNTERS/RING COUNTER/rctb.v
@@ -1,1 +1,20 @@
-
+module rctb;
+reg clk,rs;
+wire [3:0]q;
+rc uut(clk,rs,q);
+initial begin
+clk=0;
+forever #10 clk=~clk;
+end
+initial begin
+$dumpfile("rc.vcd");
+$dumpvars;
+$monitor("clk=%b,rs=%b,q=%b",clk,rs,q);
+#10 rs=0;
+#10 rs=1;
+repeat (50)begin
+end
+#70;
+$finish;
+end
+endmodule

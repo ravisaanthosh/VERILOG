b55af586c44d31a9d5581fcbe9f02fc10aaf0350
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-21 18:17:45 +0530

    Update rc.v

--- DAY-12/COUNTERS/RING COUNTER/rc.v
+++ DAY-12/COUNTERS/RING COUNTER/rc.v
@@ -1,1 +1,13 @@
+module rc(input clk,rs,output reg [3:0]q);
+always @(posedge clk)begin
+if(rs==0)
+q<=4'b1110;
+else begin
+q[3]<=q[0];
+q[2]<=q[3];
+q[1]<=q[2];
+q[0]<=q[1];
+end
+end
+endmodule
 

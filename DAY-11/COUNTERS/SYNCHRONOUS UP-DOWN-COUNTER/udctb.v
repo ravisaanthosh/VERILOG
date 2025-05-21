6e9b3a35b2b30732c8ffa40a05182a50593b482e
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-21 14:20:29 +0530

    Update udctb.v

--- DAY-11/COUNTERS/SYNCHRONOUS UP-DOWN-COUNTER/udctb.v
+++ DAY-11/COUNTERS/SYNCHRONOUS UP-DOWN-COUNTER/udctb.v
@@ -1,1 +1,19 @@
-
+module udctb;
+reg clk,rs,up;
+wire [3:0]count;
+udc uut(clk,rs,up,count);
+initial begin
+$dumpfile("coun.vcd");
+$dumpvars;
+$monitor("clk=%b,rs=%b,up=%b,count=%b",clk,rs,up,count);
+  clk=0;rs=0;
+  up=1;
+  #4; rs=1;
+  #50;
+  rs=0;
+  #4;rs=1;up=0;
+  #63;
+  $finish;
+  end
+always #2 clk=~clk;
+endmodule

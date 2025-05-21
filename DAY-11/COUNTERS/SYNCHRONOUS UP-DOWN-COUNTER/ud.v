954d340398868ba55aa21ae14f3346a77f947862
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-21 14:20:05 +0530

    Update ud.v

--- DAY-11/COUNTERS/SYNCHRONOUS UP-DOWN-COUNTER/ud.v
+++ DAY-11/COUNTERS/SYNCHRONOUS UP-DOWN-COUNTER/ud.v
@@ -1,1 +1,11 @@
-
+module udc(input clk,rs,up,output reg [3:0]count);
+always @(posedge clk)begin
+if(rs==0)begin
+count<=4'b0;
+end
+else begin
+if(up) count<=count+1'b1;
+else count<=count-1'b1;
+end
+end
+endmodule

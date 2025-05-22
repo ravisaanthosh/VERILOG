e311247c625a5129deb0bee92c73e6ea74596113
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-22 10:11:19 +0530

    Update jhc.v

--- DAY-12/COUNTERS/JHONSON COUNTER/jhc.v
+++ DAY-12/COUNTERS/JHONSON COUNTER/jhc.v
@@ -1,1 +1,24 @@
+//model-1 asper ring counter use not gate its will worked in jhonson counter
+module rc(input clk,rs,output reg [3:0]q);
+always @(posedge clk)begin
+if(rs==0)
+q<=4'b1010;
+else begin
+q[3]<=q[!0];
+q[2]<=q[3];
+q[1]<=q[2];
+q[0]<=q[1];
+end
+end
+endmodule
+//model -2 
+module rc(input clk, input rs, output reg [3:0] q);
+  always @(posedge clk) begin
+    if (rs == 0)
+      q <= 4'b1010; 
+    else
+      q <= {~q[3], q[3:1]};  
+  end
+endmodule
+
 

c0fb381391abd17ff1ac65d3639b71edc6acd194
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-27 12:52:37 +0530

    Update sram.v

--- DAY-12/S-RAM/sram.v
+++ DAY-12/S-RAM/sram.v
@@ -1,1 +1,14 @@
-
+module sram(input clk,rs,wr,input[3:0]din,input[2:0]add,output reg[3:0]dout);
+reg[3:0] ram[0:7];
+always@(posedge clk)begin
+//reset purpose
+if(rs==1)
+dout<=4'b0000;
+//write purpose
+else if(wr==1)
+ram[add]<=din;
+//read purpose
+else if(wr==0)
+dout<=ram[add];
+end
+endmodule

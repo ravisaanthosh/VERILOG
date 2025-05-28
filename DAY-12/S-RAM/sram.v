aba5600beb6ad7b15ca43d38124397278851c836
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-05-28 15:27:16 +0530

    Update sram.v

--- DAY-12/S-RAM/sram.v
+++ DAY-12/S-RAM/sram.v
@@ -1,5 +1,5 @@
 module sram(input clk,rs,wr,input[3:0]din,input[2:0]add,output reg[3:0]dout);
-reg[7:0] ram[3:0];
+  reg[7:0] ram[3:0];   //7:0 represents in adderess 3:0 represents in data
 always@(posedge clk)begin
 //reset purpose
 if(rs==1)

1ad65d1c038a506ce24bde8b3d609582c5655fb4
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-06-01 07:16:14 +0530

    Update 111tb.v

--- ASSIGNMENT/FSM/111tb.v
+++ ASSIGNMENT/FSM/111tb.v
@@ -1,1 +1,47 @@
+module seqtb;
+reg x,rst,clk;
+wire z;
+seq uut(x,rst,clk,z);
+initial clk=0;
+always #5 clk=~clk;
 
+initial begin
+$monitor($time,"x=%b,rst=%b,clk=%b,z=%b",x,rst,clk,z);
+
+$dumpfile("111.vcd");
+$dumpvars;
+
+
+#10 rst=0;
+
+#10 rst=1;
+
+
+#10 x=1;
+
+#10 x=0;
+#10 x=0;
+#10 x=0;
+#10 x=1;
+#10 x=0;
+#10 x=0;
+#10 x=0;
+#10 x=1;
+#10 x=0;
+#10 x=1;
+#10 x=1;
+#10 x=1;
+#10 x=0;
+#10 x=1;
+#10 x=1;
+
+
+
+
+
+
+
+#10
+$finish;
+end
+endmodule

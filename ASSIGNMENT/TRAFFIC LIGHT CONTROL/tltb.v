e47d301c2dbf1a5f07518fc0b8630c2a3bbbcdb1
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-06-02 19:57:23 +0530

    Update tltb.v

--- ASSIGNMENT/TRAFFIC LIGHT CONTROL/tltb.v
+++ ASSIGNMENT/TRAFFIC LIGHT CONTROL/tltb.v
@@ -1,1 +1,17 @@
-
+module tftb;
+reg clk,rs;
+wire [2:0]north_light,south_light,east_light,west_light;
+tf uut(clk,rs,north_light,south_light,east_light,west_light);
+initial begin
+$dumpfile("tflight.vcd");
+$dumpvars;
+$monitor($time,"clk=%b,rs=%b,north_light=%b,south_light=%b,east_light=%b,west_light=%b",clk,rs,north_light,south_light,east_light,west_light);
+clk=0;
+forever #5 clk=~clk;
+end
+initial begin
+rs=1;
+#15 rs=0;
+#110 $finish;
+end
+endmodule

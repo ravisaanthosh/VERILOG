e495588fd1f2775814d4ccf663028e76fe200af5
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-06-03 14:18:27 +0530

    Update nf.v

--- ASSIGNMENT/FSM_USING_ON_&_OFF/nf.v
+++ ASSIGNMENT/FSM_USING_ON_&_OFF/nf.v
@@ -1,1 +1,18 @@
+module jk(input clk,rs,j,k,output reg out);
+parameter off=1'b0;
+parameter on=1'b1;
+reg state;
+always @(posedge clk or posedge rs)begin
+if(rs)
+state<=off;
 
+else
+state<=(state==off)?(j?on:off):(k?off:on);
+end
+always @(*)begin
+if(state==off)
+out=1'b0;
+else
+out=1'b1;
+end
+endmodule

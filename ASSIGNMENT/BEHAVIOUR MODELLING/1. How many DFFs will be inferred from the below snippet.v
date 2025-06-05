e3fa10f1311f2a0e0371e1219a7019af2e0a86ca
SANTHOSH R <ravisanthosh0662@gmail.com>
2025-06-05 15:02:01 +0530

    Create 1. How many DFFs will be inferred from the below snippet.v

---
+++ ASSIGNMENT/BEHAVIOUR MODELLING/1. How many DFFs will be inferred from the below snippet.v
@@ -0,0 +1,12 @@
+input c, din;
+output reg y3;
+reg y1,y2;
+always@ ( posedge c) begin
+y1 = din;
+y2 = y1;
+y3 = y2;
+end
+
+
+answer 1
+because its blocking assignment so y1=din y2=y1 y3=y2 its executive line by line
